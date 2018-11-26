564da7732ecc01a1ecbfbb5b2387c013b823d877
Ács Bence Tamás <acs.bence.tamas@gmail.com>
2018-11-26 13:37:01 +0100

    connections read_all updated

--- connection.py
+++ connection.py
@@ -1,21 +1,10 @@
 import csv
 
-def read_all(file_path, header_list):
+
+def read_all(file_path):
     all_data = []
     with open(file_path) as data_base:
-        csv_reader = csv.reader(data_base)
+        csv_reader = csv.DictReader(data_base)
         for record in csv_reader:
-            if len(record) > len(header_list):
-                raise IndexError("Header length and record lengths do not match.")
-            record_as_dictionary = {}
-            for index, item in enumerate(record):
-                try:
-                    item = int(item)
-                except ValueError:
-                    try:
-                        item = float(item)
-                    except ValueError:
-                        pass
-                record_as_dictionary[header_list[index]] = item
-            all_data.append(record_as_dictionary)
+            all_data.append(record)
     return all_data
--- data_manager.py
+++ data_manager.py
@@ -1,7 +1,7 @@
 import os
+import connection
 
 QUESTIONS_FILE_PATH = os.getenv('QUESTIONS_FILE_PATH') if 'QUESTIONS_FILE_PATH' in os.environ else 'sample_data/question.csv'
 ANSWERS_FILE_PATH = os.getenv('ANSWERS_FILE_PATH') if 'ANSWERS_FILE_PATH' in os.environ else 'sample_data/answer.csv'
 QUESTIONS_HEADER = ["id", "submission_time", "view_number", "vote_number", "title", "message", "image"]
 ANSWERS_HEADER = ["id", "submission_time", "vote_number", "question_id", "message", "image"]
-
